4a3b1096dfc1f1205d66643561cdbfa0ab38183e
Ali Moradi\Ali <moradi1101@outlook.com>
2023-02-09 07:56:45 +0330

    remove extra

--- BulkCopyFromExcel/wwwroot/js/UploadImage.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    $(".custom-file-input").on("change",
-        function () {
-            var filename = $(this).val().split('\\').pop();
-            $(this).next('.custom-file-label').html(filename);
-        });
-});
